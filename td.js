c94fef7f12da4d2b9876da2034a4bf889bda02d1
Thiago Barcala <barcala@code4business.de>
2014-09-16 09:43:49 +0200

    Final

--- td.js
+++ td.js
@@ -18,11 +18,13 @@
     var canvas = document.getElementById("GameView");
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
+    
+    var startLevel = 0;
 
     var Game = function() {
         var score = 0;
-        var money = 200;
-        var lives = 2;
+        var money = 200 + startLevel * 150;
+        var lives = 100;
         var frameController = new FrameController(canvas, STEPS_PER_SECOND, 60);
         var FieldSize = {
             width : 35,
@@ -30,10 +32,10 @@
         };
         var BLOCK_SIZE = 25;
         var CREEP_RADIUS = 0.09;
-        var fieldOffset = { x: -BLOCK_SIZE, y: 0 };
+        var fieldOffset = { x: -BLOCK_SIZE, y: 40 };
         var overListeners = [];
         var game = this;
-        var mousePosition;
+        var mousePosition = null;
 
         // Represents the field where the game happens.
         var Field = function(width, height) {
@@ -143,7 +145,9 @@
                 var gc = image.getContext("2d");
                 for (var i = 0; i < width; i++) {
                     for (var j = 0; j < height; j++) {
-                        gc.fillStyle = [ "green", "brown", "gray" ][cells[i][j]];
+                        var c = cells[i][j];
+                        if (c instanceof Tower) c = 2;
+                        gc.fillStyle = [ "green", "brown", "gray" ][c];
                         gc.fillRect(i * BLOCK_SIZE, j * BLOCK_SIZE, BLOCK_SIZE,
                                 BLOCK_SIZE);
                     }
@@ -168,49 +172,68 @@
                 y : height / 2 | 0
             };
 
-            // Checks if the position can be occupied, and if so mark it as occupied.
-            this.putTower = function(x, y) {
+            function reduceNeighbors(x, y, f) {
+                var r;
+                for (var i = 0; i < 2; i++) {
+                    for (var j = 0; j < 2; j++) {
+                        r = f(x + i, y + j);
+                        if (r && r.end) return r.value;
+                    }
+                }
+                return r && r.value;
+            }
+            
+            // Checks if the position can be occupied.
+            this.canPutTower = function(x, y) {
                 if (x <= 0 || y <= 0 || x >= width - 2 || y >= height - 2)
                     return false;
                 
-                function reduceNeighbors(f) {
-                    var r;
-                    for (var i = 0; i < 2; i++) {
-                        for (var j = 0; j < 2; j++) {
-                            r = f(x + i, y + j);
-                            if (r && r.end) return r.value;
-                        }
-                    }
-                    return r && r.value;
-                }
-                
-                var someCellOccupied = reduceNeighbors(function (i, j) {
+                var someCellOccupied = reduceNeighbors(x, y, function (i, j) {
                     var cellOccupied = cells[i][j];
                     return { end: cellOccupied, value: cellOccupied };
                 });
                 if (someCellOccupied)
                     return;
-                reduceNeighbors(function (i, j) {
+                reduceNeighbors(x, y, function (i, j) {
                     cells[i][j] = 2;
                 });
                 
                 var previous = dijkstra(this.goal);
+                reduceNeighbors(x, y, function (i, j) {
+                    cells[i][j] = 0;
+                });
                 if (previous[this.origin.x][this.origin.y]) {
-                    this.previous = previous;
-                    backgroundImage = renderBackground();
                     return true;
                 } else {
-                    reduceNeighbors(function (i, j) {
-                        cells[i][j] = 0;
-                    });
                     return false;
                 }
             };
             
-            this.translate = function (p) {
+            // Put a tower in the position x, y.
+            this.putTower = function (tower, x, y) {
+                reduceNeighbors(x, y, function (i, j) {
+                    cells[i][j] = 2;
+                });
+                cells[x][y] = tower;
+                this.previous = dijkstra(this.goal);
+                backgroundImage = renderBackground();
+            };
+            
+            this.hasTower = function (p) {
+                return cells[p.x][p.y] instanceof Tower && cells[p.x][p.y]; 
+            };
+            
+            this.map = function (p) {
                 return {
                     x: Math.round((p.x - fieldOffset.x) / BLOCK_SIZE - 1),
                     y: Math.round((p.y - fieldOffset.y) / BLOCK_SIZE - 1)
+                };
+            };
+            
+            this.unmap = function (p) {
+                return {
+                    x: (p.x + 1) * BLOCK_SIZE + fieldOffset.x,
+                    y: (p.y + 1) * BLOCK_SIZE + fieldOffset.y
                 };
             };
 
@@ -223,6 +246,46 @@
 
             this.previous = dijkstra(this.goal);
         };
+        
+        // Represents a tower.
+        var Tower = function(position, createBullet) {
+            var bulletRange = 3;
+            var BULLET_SPEED = 7 / STEPS_PER_SECOND;
+            var bulletDuration = bulletRange / BULLET_SPEED | 0;
+            var fireDelay = 1 * STEPS_PER_SECOND;
+            var fireCounter = 0;
+
+            // Tries to find a target, aim and shoot.
+            this.step = function() {
+                if (!fireCounter) {
+                    var target = creepsManager.getCreepAtDistance(position, bulletRange);
+                    if (!target)
+                        return;
+                    var distance = Vector.subFrom(position, Vector.copy(target.getPosition()));
+                    var offset = Vector.scale(Vector.copy(target.getSpeed()), Vector.norm(distance) / BULLET_SPEED);
+                    Vector.addTo(offset, distance);
+                    Vector.scale(distance, BULLET_SPEED / Vector.norm(distance));
+                    createBullet(Vector.copy(position), distance, bulletDuration, 1 + this.level);
+                    fireCounter = fireDelay;
+                } else {
+                    fireCounter--;
+                }
+            };
+            
+            this.render = function (gc) {
+                gc.fillStyle = "yellow";
+                gc.fillRect(position.x - 1, position.y - 1, this.level * 0.4, 3 / BLOCK_SIZE);
+            };
+            
+            this.level = 1;
+            
+            this.upgrade = function () {
+                fireDelay = fireDelay * 0.7 | 0;
+                bulletRange += 0.8;
+                bulletDuration = bulletRange / BULLET_SPEED | 0;
+                this.level++;
+            };
+        };
 
         var field = new Field(FieldSize.width, FieldSize.height);
 
@@ -230,7 +293,7 @@
         var CreepsManager = function(origin) {
             var creeps = [];
             var summoning = false;
-            var level = 0;
+            var level = startLevel;
             var creepsToSummon = 30;
             var summonCounter = 0;
             var summonDelay = STEPS_PER_SECOND / 2;
@@ -354,7 +417,7 @@
             var position = Vector.copy(p);
             var speed = s;
             var currentCell = Vector.round(Vector.copy(position));
-            var life = 5 + level * level;
+            var life = 5 + 1 * level * level;
 
             // Renders the creep.
             this.render = function(gc) {
@@ -447,11 +510,12 @@
             var bullets = [];
 
             // Adds a new bullet.
-            this.createBullet = function(position, speed, duration) {
+            this.createBullet = function(position, speed, duration, damage) {
                 bullets.push({
                     position : position,
                     speed : speed,
-                    duration : duration
+                    duration : duration,
+                    damage: damage
                 });
             };
 
@@ -462,7 +526,7 @@
                     var creep = creepsManager.getCreepAtDistance(
                             bullet.position, CREEP_RADIUS);
                     if (creep)
-                        creep.inflictDamage(1);
+                        creep.inflictDamage(bullet.damage);
                     if (!bullet.duration-- || creep)
                         bullets.splice(i, 1);
                     else
@@ -474,54 +538,140 @@
             this.render = function(gc) {
                 gc.fillStyle = "white";
                 bullets.forEach(function(bullet) {
-                    gc.fillRect(bullet.position.x, bullet.position.y, 0.025,
-                            0.025);
+                    gc.fillRect(bullet.position.x, bullet.position.y, 1 / BLOCK_SIZE, 1 / BLOCK_SIZE);
                 });
             };
         };
+        
+        var Button = function (rect, label, visible, onClick, onRender) {
+            var clickCount = 0;
+            
+            this.rect = rect;
+            
+            this.render = function (gc) {
+                var bright = 192 * clickCount / STEPS_PER_SECOND | 0;
+                gc.fillStyle = "rgb(255, " + bright + ", " + bright + ")";
+                gc.save();
+                gc.translate(rect.x, rect.y);
+                gc.fillRect(0, 0, rect.width, rect.height);
+                gc.translate(rect.width / 2, rect.height-5);
+                gc.textAlign = "center";
+                gc.textBaseline = "bottom";
+                gc.fillStyle = "white";
+                gc.fillText(label, 0, 0);
+                gc.translate(-rect.width / 2, rect.height);
+                gc.scale(rect.width, rect.height);
+                onRender(gc);
+                gc.restore();
+            };
 
-        // Represents a tower.
-        var Tower = function(position, createBullet) {
-            var BULLET_RANGE = 3;
-            var BULLET_SPEED = 7 / STEPS_PER_SECOND;
-            var BULLET_DURATION = BULLET_RANGE / BULLET_SPEED | 0;
-            var fireDelay = 0.2 * STEPS_PER_SECOND;
-            var fireCounter = 0;
-
-            // Tries to find a target, aim and shoot.
-            this.step = function() {
-                if (!fireCounter) {
-                    var target = creepsManager.getCreepAtDistance(position, BULLET_RANGE);
-                    if (!target)
+            this.click = function () {
+                clickCount = STEPS_PER_SECOND;
+                onClick();
+            };
+            
+            this.step = function () {
+                if (clickCount) clickCount--;
+            };
+            
+            this.visible = visible;
+        };
+        
+        var buttonsManager = new function () {
+            var buttons = [];
+            
+            this.addButton = function (button) {
+                buttons.push(button);
+            };
+            
+            this.render = function (gc) {
+                buttons.forEach(function (b) {
+                    if (b.visible) b.render(gc);
+                });
+            };
+            
+            this.step = function () {
+                buttons.forEach(function (b) {
+                    if (!b.visible)
                         return;
-                    var distance = Vector.subFrom(position, Vector.copy(target.getPosition()));
-                    var offset = Vector.scale(Vector.copy(target.getSpeed()), Vector.norm(distance) / BULLET_SPEED);
-                    Vector.addTo(offset, distance);
-                    Vector.scale(distance, BULLET_SPEED / Vector.norm(distance));
-                    createBullet(Vector.copy(position), distance, BULLET_DURATION);
-                    fireCounter = fireDelay;
-                } else {
-                    fireCounter--;
-                }
+                    var click = frameController.readMouseClick(b.rect);
+                    if (click) {
+                        b.click();
+                    };
+                    b.step();
+                });
             };
         };
+        
+        var btnSendCreeps = new Button({ x: 10, y: 5, width: 120, height: BLOCK_SIZE }, "Send creeps!", true, 
+            function () {
+                game.creepsManager.startSummoning();
+            }, 
+            function () {});
+
+        var btnUpgrade = new Button({ x: 0, y: 0, width: BLOCK_SIZE * 2, height: BLOCK_SIZE }, "Upgr. $50", false, 
+            function () {
+                if (money < 50)
+                    return;
+                money -= 50;
+                btnUpgrade.tower.upgrade();
+            }, function () {
+                
+            });
+        
+        var towers = [];
+        var btnCreateTower = new Button({ x: 0, y: 0, width: BLOCK_SIZE * 2, height: BLOCK_SIZE }, "Tower $45", false, 
+            function () {
+                if (money < 45)
+                    return;
+                var tower = new Tower(Vector.addTo(btnCreateTower.p, { x: 0.5, y: 0.5 }), bulletsManager.createBullet);
+                towers.push(tower);
+                field.putTower(tower, btnCreateTower.p.x, btnCreateTower.p.y);
+                frameController.addActionObject(tower);
+                money -= 45;
+                btnCreateTower.visible = false;
+            }, function () {
+                
+            });
+
+        buttonsManager.addButton(btnSendCreeps);
+        buttonsManager.addButton(btnUpgrade);
+        buttonsManager.addButton(btnCreateTower);
 
         var creepsManager = new CreepsManager(field.origin);
         var bulletsManager = new BulletsManager();
         
         frameController.addActionObject(creepsManager);
         frameController.addActionObject(bulletsManager);
+        frameController.addActionObject(buttonsManager);
         frameController.addActionObject(new function() {
             this.step = function() {
                 var click;
                 while (click = frameController.readMouseClick()) {
-                    var p = field.translate(click);
-                    mousePosition = p;
-                    if (money < 45)
-                        continue;
-                    if (field.putTower(p.x, p.y)) {
-                        frameController.addActionObject(new Tower(p, bulletsManager.createBullet));
-                        money -= 45;
+                    var p = field.map(click);
+                    var m = p;
+                    m.x = Math.max(2, Math.min(m.x, FieldSize.width - 5));
+                    m.y = Math.max(1, Math.min(m.y, FieldSize.height - 3));
+                    btnCreateTower.visible = false;
+                    btnUpgrade.visible = false;
+                    if (mousePosition && Vector.equals(m, mousePosition)) {
+                        mousePosition = null;
+                    } else {
+                        mousePosition = m;
+                        var tower;
+                        if (tower = field.hasTower(p)) {
+                            if (tower.level < 5) {
+                                btnUpgrade.visible = true;
+                                btnUpgrade.rect.x = field.unmap(m).x - BLOCK_SIZE;
+                                btnUpgrade.rect.y = field.unmap(m).y + BLOCK_SIZE;
+                                btnUpgrade.tower = tower;
+                            }
+                        } else if (field.canPutTower(p.x, p.y)) {
+                            btnCreateTower.visible = true;
+                            btnCreateTower.rect.x = field.unmap(m).x - BLOCK_SIZE;
+                            btnCreateTower.rect.y = field.unmap(m).y + BLOCK_SIZE;
+                            btnCreateTower.p = p;
+                        }
                     }
                 }
             };
@@ -530,6 +680,7 @@
         frameController.addRenderObject(field);
         frameController.addRenderObject({
             render: function (gc) {
+                gc.clearRect(0, 0, canvas.width, 40);
                 gc.save();
                 gc.translate(fieldOffset.x, fieldOffset.y);
                 gc.scale(BLOCK_SIZE, BLOCK_SIZE);
@@ -544,9 +695,17 @@
         frameController.addRenderObject(bulletsManager);
         frameController.addRenderObject({
             render: function (gc) {
+                towers.forEach(function (t) {
+                    t.render(gc);
+                });
+            }
+        });
+        frameController.addRenderObject({
+            render: function (gc) {
                 gc.restore();
             }
         });
+        frameController.addRenderObject(buttonsManager);
         
         frameController.addRenderObject(new function() {
             this.render = function(gc) {
@@ -577,10 +736,7 @@
         this.creepsManager = creepsManager;
     };
     
-    var game;
-    document.getElementById("btnSendCreeps").addEventListener("click", function () {
-        game.creepsManager.startSummoning();
-    });
+    var game = null;
     
     document.getElementById("btnNewGame").addEventListener("click", function (e) {
         var gameMenu = document.getElementById("GameMenu");
@@ -596,7 +752,7 @@
                 document.getElementById("Highscore").innerHTML = score;
             }
             document.getElementById("Score").innerHTML = score;
-            document.getElementById("Scores").style = "";
+            document.getElementById("Scores").style.opacity = "";
             gameMenu.style.zIndex = "";
             gameMenu.classList.toggle("transparent");
         });
